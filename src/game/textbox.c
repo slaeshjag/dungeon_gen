c4b9c22478fce8c7855a9ae0219e45d23682a6b8
Steven Arnow <s@rdw.se>
2013-10-01 01:50:30 +0200

    Worked some more on textboxes

--- src/game/textbox.c
+++ src/game/textbox.c
@@ -1,3 +1,4 @@
+#define	_EMIT_PALETTE
 #include "textbox.h"
 #include "world.h"
 
@@ -16,9 +17,12 @@ void textbox_init(unsigned int w, unsigned int h, int x, int y) {
 	tb->options = 0;
 
 	tb->face = NULL;
-	tb->text = d_text_surface_new(ws.font, 2048, w, x, y);
+	tb->text = d_text_surface_color_new(ws.font, 2048, w, x, y);
 	tb->rows = h / d_font_glyph_hs(ws.font);
 	tb->surface_w = w;
+
+	tb->ms_per_char = 200;
+	tb->dt = 0;
 
 	ws.textbox = tb;
 
@@ -28,6 +32,8 @@ void textbox_init(unsigned int w, unsigned int h, int x, int y) {
 
 void textbox_loop() {
 	struct textbox *tb;
+	unsigned char *p = textbox_color_palette;
+	int i;
 
 	/* TODO: Init */
 	tb = ws.textbox;
@@ -35,13 +41,28 @@ void textbox_loop() {
 		return;
 	
 	/* Improve or somth.. */
-	if (tb->message[tb->char_pos]) {
-		if (tb->message[tb->char_pos] == ' ') {
-			tb->char_pos++;
-			if (d_text_surface_pos(tb->text) + d_font_word_w(ws.font, &tb->message[tb->char_pos], NULL) >= tb->surface_w)
-				d_text_surface_char_append(tb->text, "\n");
-		} else
-			tb->char_pos += d_text_surface_char_append(tb->text, &tb->message[tb->char_pos]);
+	tb->dt += d_last_frame_time();
+	for (; tb->dt > tb->ms_per_char && tb->message[tb->char_pos]; tb->dt -= tb->ms_per_char) {
+		if (tb->message[tb->char_pos]) {
+			if (tb->message[tb->char_pos] == ' ') {
+				tb->char_pos++;
+				if (d_text_surface_pos(tb->text) + d_font_word_w(ws.font, 
+				    &tb->message[tb->char_pos], NULL) >= tb->surface_w)
+					d_text_surface_char_append(tb->text, "\n");
+			} else if (tb->message[tb->char_pos] == '\x01') {
+				tb->char_pos++;
+				i = (tb->char_pos << 2);
+				d_text_surface_color_next(tb->text, p[i], p[i+1], p[i+2]);
+				tb->char_pos++;
+			} else if (tb->message[tb->char_pos] == '\x02') {
+				tb->char_pos++;
+				i = ((unsigned) tb->message[tb->char_pos]);
+				i <<= 2;
+				tb->ms_per_char = i;
+			} else
+				tb->char_pos += d_text_surface_char_append(tb->text, 
+					&tb->message[tb->char_pos]);
+		}
 	}
 
 	return;
