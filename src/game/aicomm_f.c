dc808227cfd484766c9d6e2fadb603db75bf74c4
Steven Arnow <s@rdw.se>
2013-11-02 22:07:48 +0100

    Ficzed spawn message

--- src/game/aicomm_f.c
+++ src/game/aicomm_f.c
@@ -96,7 +96,7 @@ struct aicomm_struct aicomm_f_spwn(struct aicomm_struct ac) {
 
 	x = ac.arg[1] * ws.camera.tile_w;
 	y = ac.arg[2] * ws.camera.tile_h;
-	character_spawn_entry(ac.arg[0], ac.argp, x, y, ac.arg[3], 4, -1);
+	character_spawn_entry(ac.arg[0], ac.argp, x, y, ac.arg[3], ws.dm->grid[4].id, -1);
 
 	return character_message_next(ac);
 }
