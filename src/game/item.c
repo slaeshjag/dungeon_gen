a120f1b83f8e6c4b27b8d035a50cffe3e21a6fb2
Steven Arnow <s@rdw.se>
2013-11-05 19:09:07 +0100

    Started on item framework

--- src/game/item.c
+++ src/game/item.c
@@ -1,5 +1,54 @@
 #include "savefile.h"
 #include "item.h"
+#include <stdlib.h>
+#include <darnit/darnit.h>
 
 
+struct item *item_init(const char *item_table) {
+	struct item *item;
+	DARNIT_FILE *f;
 
+	if (!(f = d_file_open(item_table, "rb")))
+		return NULL;
+	item = malloc(sizeof(*item));
+
+	/* TODO: Read item table */
+	item->type = NULL;
+	item->types = 0;
+
+	d_file_close(f);
+
+	return item;
+}
+
+
+struct item *item_destroy(struct item *item) {
+	int i;
+
+	for (i = 0; i < item->types; i++)
+		free(item->type[i].description), free(item->type[i].name);
+	free(item->type);
+	free(item);
+
+	return NULL;
+}
+
+
+struct inventory *inventory_new(int size) {
+	struct inventory *inv;
+	int i;
+
+	inv = malloc(sizeof(*inv));
+	inv->entry = malloc(sizeof(*inv->entry) * size);
+	for (i = 0; i < size; i++)
+		inv->entry[i].type = inv->entry[i].amount = -1;
+	return inv;
+}
+
+
+struct inventory *inventory_destroy(struct inventory *inv) {
+	free(inv->entry);
+	free(inv);
+
+	return NULL;
+}
--- src/game/item.h
+++ src/game/item.h
@@ -2,7 +2,38 @@
 #define __ITEM_H__
 
 
+struct item_type {
+	char				*name;
+	char				*description;
+	/* TODO: Insert function pointer for handler */
+	int				datavalue;
+	int				max_stack;
+};
 
+
+struct item {
+	struct item_type		*type;
+	int				types;
+};
+
+
+struct inventory_entry {
+	/* -1 == unused */
+	int				type;
+	int				amount;
+};
+
+
+struct inventory {
+	struct inventory_entry		*entry;
+	int				entries;
+};
+
+
+struct item *item_init(const char *item_table);
+struct item *item_destroy(struct item *item);
+struct inventory *inventory_new(int size);
+struct inventory *inventory_destroy(struct inventory *inv);
 
 
 #endif
